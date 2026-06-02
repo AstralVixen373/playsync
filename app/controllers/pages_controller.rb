fb30fc82e35ac13ca75893e2cb61ad77f198d395
Theo Soubra Belay <TheoSoubraBelay@users.noreply.github.com>
2026-06-02 15:10:04 +0200

    views+controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @posts = Post.includes(:user).order(created_at: :desc).limit(6)
   end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -13,26 +13,26 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
 
   test "should get new" do
     get posts_new_url
-    assert_response :success
+    assert_redirected_to root_url
   end
 
   test "should get create" do
     get posts_create_url
-    assert_response :success
+    assert_redirected_to root_url
   end
 
   test "should get edit" do
     get posts_edit_url
-    assert_response :success
+    assert_redirected_to root_url
   end
 
   test "should get update" do
     get posts_update_url
-    assert_response :success
+    assert_redirected_to root_url
   end
 
   test "should get destroy" do
     get posts_destroy_url
-    assert_response :success
+    assert_redirected_to root_url
   end
 end
