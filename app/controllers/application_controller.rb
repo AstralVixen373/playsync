9066b5c05c9979b0f08c978352b392e1f002997c
Cyril Faccin <Cyril.faccin@outlook.fr>
2026-06-04 14:53:43 +0200

    avatars et usernames fonctionnels

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
   include Pundit::Authorization
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   # Pundit: allow-list approach
@@ -21,7 +22,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar])
