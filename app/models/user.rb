b647d632f3ddf708c968d233f5d931f0261f4028
Cyril Faccin <Cyril.faccin@outlook.fr>
2026-06-08 10:05:53 +0200

    premières réflexions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
+         :omniauthable, omniauth_providers: [:steam]
 
   has_many :posts, dependent: :destroy
   has_many :messages, dependent: :destroy
@@ -12,4 +12,12 @@ class User < ApplicationRecord
   has_many :chats, through: :user_chats
 
   has_one_attached :avatar
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
+      user.email      = auth.info.email
+      user.password   = Devise.friendly_token[0, 20]
+      user.avatar_url = auth.info.image
+    end.tap(&:save!)
+  end
 end
