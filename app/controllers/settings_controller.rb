81faa95ba9de337c38edc70a58e5779900858f48
Lucie Aymeric <luciea3011@gmail.com>
2026-06-08 11:34:56 +0200

    Moved the account section from Profile to Settings and changed the hardcoded colours to bootstrap variables to implement the themes feature

---
+++ app/controllers/settings_controller.rb
@@ -0,0 +1,35 @@
+class SettingsController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+  end
+
+  def update_email
+    if current_user.update(email: params[:user][:email])
+      redirect_to settings_path, notice: "Email updated successfully."
+    else
+      redirect_to settings_path, alert: current_user.errors.full_messages.join(", ")
+    end
+  end
+
+  def update_theme
+    theme = params[:theme].to_s
+    if %w[light dark].include?(theme)
+      current_user.update!(theme: theme)
+    end
+    redirect_to settings_path
+  end
+
+  def update_password
+    unless current_user.valid_password?(params[:user][:current_password])
+      return redirect_to settings_path, alert: "Current password is incorrect."
+    end
+
+    if current_user.update(password: params[:user][:password], password_confirmation: params[:user][:password_confirmation])
+      bypass_sign_in(current_user)
+      redirect_to settings_path, notice: "Password updated successfully."
+    else
+      redirect_to settings_path, alert: current_user.errors.full_messages.join(", ")
+    end
+  end
+end
