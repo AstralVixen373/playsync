b241983e0da47a105115079bba9b87011328993c
guillaume dumaine <sydberny@gmail.com>
2026-06-09 17:21:00 +0200

    turbo participant et players slots

--- app/models/user_chat.rb
+++ app/models/user_chat.rb
@@ -1,4 +1,23 @@
 class UserChat < ApplicationRecord
   belongs_to :user
   belongs_to :chat
+
+  after_create_commit :broadcast_append_to_user_chat
+  after_destroy_commit :broadcast_remove_to_user_chat
+
+  private
+
+  def broadcast_append_to_user_chat
+    broadcast_append_to "user_chat", target: "chat_users", partial: "posts/chat_user", locals: { user: user }
+    update_players_slot
+  end
+
+  def broadcast_remove_to_user_chat
+    broadcast_remove_to "user_chat", target: "user_#{user.id}"
+    update_players_slot
+  end
+
+  def update_players_slot
+    broadcast_update_to "user_chat", target: "post_#{chat.post.id}_slots", html: "<strong>#{chat.post.members_count}/#{chat.post.capacity}</strong> players"
+  end
 end
