0367dcc82ae7a17ce39081150965a3540c591c2f
Cyril Faccin <Cyril.faccin@outlook.fr>
2026-06-12 09:32:15 +0200

    DRY

--- app/javascript/popup.js
+++ app/javascript/popup.js
@@ -2,14 +2,19 @@ const buttons = document.querySelectorAll("#button");
 const confirm = document.getElementById("confirm");
 const text = document.getElementById("text");
 
-const temp = (button) => ({
-  if (button = "Mark as finished") {
-    console.log(document.getElementById("finishing"));
-    return document.getElementById("finishing").innerHTML;
-  }, elsif (button = "Leave match") {
-    return document.getElementById("leaving").innerHTML;
-  }, elsif (button = "Delete") {
-    return document.getElementById("deleting").innerHTML;
+const temp = (code) => ({
+  if (code = "Mark as finished") {
+    const answer = document.getElementById("finishing");
+    console.log(answer);
+    return answer;
+  }, elsif (code = "Leave match") {
+    const answer = document.getElementById("leaving");
+    console.log(answer);
+    return answer;
+  }, elsif (code = "Delete") {
+    const answer = document.getElementById("deleting");
+    console.log(answer);
+    return answer;
   }
 });
 
@@ -17,11 +22,10 @@ buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     event.preventDefault();
     text.replaceChildren();
-    const template = temp(button.innerText);
-    text.insertAdjacentHTML("beforeend", `<img src="app/assets/images/playsync-logo.png" alt="PlaySync logo">`);
-    text.insertAdjacentHTML("beforeend", `<h1>PlaySync indicates :</h1>`);
+    const content = button.innerText;
+    const template = temp(content);
     text.insertAdjacentHTML("beforeend", `<p>Do you want to ${button.innerText}?</p>`);
-    text.insertAdjacentHTML("beforeend", `${template}`);
+    text.insertAdjacentHTML("beforeend", `<div class="d-flex justify-content-between gap-4">${template}<div>`);
     confirm.showModal();
   });
 });
